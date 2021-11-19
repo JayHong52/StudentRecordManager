a3ca58f8f91f25ab9677bb988bd7d5e9b41df52a
JayHong52 <jhong52@my.centennialcollege.ca>
2021-11-19 18:59:16 -0500

    Initial Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.exercise.jiwoonghong_comp228lab4 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+
+    opens com.exercise1 to javafx.fxml;
+    exports com.exercise1;
+}
